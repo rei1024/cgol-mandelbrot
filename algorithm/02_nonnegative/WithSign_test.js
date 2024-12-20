d909e837023eb31537d6b8eee58743ab14ab8600
rei1024 <satopen1729@gmail.com>
2024-12-21 01:34:43 +0900

    feat: remove test file

--- algorithm/02_nonnegative/WithSign_test.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-
-import { test, assertEquals } from "../../test/deps.js";
-import { WithSign } from "./WithSign.js";
-
-test("WithSign", () => {
-    assertEquals(WithSign.zero().succ().asBigInt(), 1n)
-});
